44b490f8304e90999d0c57afefcae585c8056885
grossmann4 <grossmann4@yahoo.com>
2023-09-14 17:12:11 -0400

    added springboot files

---
+++ src/main/java/com/FantasyChess/FantasyAPI/FantasyApiApplication.java
@@ -0,0 +1,13 @@
+package com.FantasyChess.FantasyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyApiApplication.class, args);
+	}
+
+}
